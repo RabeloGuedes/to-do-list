fa026a0bfed8a50bd4d3ec2e00739fa636e8d68e
RabeloGuedes <krepscredian@gmail.com>
2022-02-16 16:28:52 -0300

    feat: Desenvolve no script a lógica que ao adicionar ou clicar em um item da lista, o deixa com o fundo cinza e todos os outros com o fundo branco.

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const addTaskButton = document.querySelector('#criar-tarefa');
 const orderedList = document.querySelector('#lista-tarefas');
 const inputTask = document.querySelector('#texto-tarefa');
+
 addTaskButton.addEventListener('click', function () {
   let newItem = document.createElement('li');
+  const itensList = document.querySelectorAll('.itens-list');
+  for (let index = 0; index < itensList.length; index += 1) {
+    itensList[index].className = 'itens-list';
+  }
+  newItem.className = 'itens-list gray';
   newItem.innerText = inputTask.value;
   orderedList.appendChild(newItem);
   inputTask.value = '';
+})
+
+orderedList.addEventListener('click', function (event) {
+  const itensList = document.querySelectorAll('.itens-list');
+  if (event.target != orderedList) {
+    for (let index = 0; index < itensList.length; index += 1) {
+      itensList[index].style.backgroundColor = 'white';
+    }
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
 })
