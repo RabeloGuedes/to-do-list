028802edb6beb2a6cf06a6c8b474f4408620e450
RabeloGuedes <krepscredian@gmail.com>
2022-02-16 16:53:10 -0300

    fix && feat: Modifica a forma de selecionar os itens e adiciona lógica de tracejar ou remover tracejo de um item ao clicar duas vezes sobre ele.

--- script.js
+++ script.js
@@ -4,22 +4,33 @@ const inputTask = document.querySelector('#texto-tarefa');
 
 addTaskButton.addEventListener('click', function () {
   let newItem = document.createElement('li');
-  const itensList = document.querySelectorAll('.itens-list');
+  const itensList = document.querySelectorAll('li');
   for (let index = 0; index < itensList.length; index += 1) {
-    itensList[index].className = 'itens-list';
+    itensList[index].style.backgroundColor = 'white';
   }
-  newItem.className = 'itens-list gray';
+  newItem.style.backgroundColor = 'rgb(128, 128, 128)';
   newItem.innerText = inputTask.value;
   orderedList.appendChild(newItem);
   inputTask.value = '';
 })
 
 orderedList.addEventListener('click', function (event) {
-  const itensList = document.querySelectorAll('.itens-list');
+  const itensList = document.querySelectorAll('li');
   if (event.target != orderedList) {
     for (let index = 0; index < itensList.length; index += 1) {
       itensList[index].style.backgroundColor = 'white';
     }
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+})
+
+orderedList.addEventListener('dblclick', function (event) {
+  const itensList = document.querySelectorAll('li');
+  if (event.target != orderedList) {
+    if (event.target.className != 'completed') {
+      event.target.className = 'completed';
+    } else {
+      event.target.className = 'itens-list';
+    }
   }
 })
